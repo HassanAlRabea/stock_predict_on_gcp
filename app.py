906fc2e31c5145a763c05859ed864d320f55b546
HassanAlRabea <alrabea.hassan@gmail.com>
2020-10-18 16:28:28 -0400

    trying to cont. deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Testing auto run deployment'
+    return 'Hello World! Testing auto run deployment - Trial number 2'
 
 
 if __name__ == '__main__':
