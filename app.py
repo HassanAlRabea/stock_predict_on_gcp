4aa02de737bcab16e7402649883aa890ac06e86f
HassanAlRabea <alrabea.hassan@gmail.com>
2020-10-25 14:20:57 -0400

    removed the main init file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from process_query import process_data
 
 @app.route('/')
 def hello_world():
-    prediction = process_data('nflx').to_string(header = True, index = False)
+    prediction = process_data('aapl').to_string(header = True, index = False)
     return prediction
 
 if __name__ == '__main__':
