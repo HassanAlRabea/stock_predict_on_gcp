b4c3afd4f1bddbf23656487dd09a53d2ac566c73
HassanAlRabea <alrabea.hassan@gmail.com>
2020-11-03 00:47:03 -0500

    switch model to Random forest classifer

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import pandas as pd
 app = Flask(__name__)
 
 from src.business_logic.process_query import process_data
+from src.business_logic.process_query import process_data_clf
 from src.business_logic.process_query import balanced_accuracy_result
+from src.business_logic.process_query import balanced_accuracy_result_clf
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -12,14 +14,17 @@ def hello():
 
 @app.route('/get_stock_val/<ticker>', methods=['GET'])
 def get_stock_value(ticker):
-    prediction = process_data(ticker)
-    # .to_string(header = True, index = False)
-    pred_price = prediction['predicted_price'].to_string(header = True, index = False)
-    recommendation = prediction['recommendation'].to_string(header = True, index = False)
-    ba_score = str(round(balanced_accuracy_result() * 100,2)) + " %"
-    answer = "Tomorrow's predicted price for " + str(ticker) + " is " + str(pred_price) \
+    prediction = process_data_clf(ticker)
+
+    # pred_price = prediction['predicted_price'].to_string(header = True, index = False)
+    recommendation = prediction[1]
+    # ba_score = str(round(balanced_accuracy_result() * 100,2)) + " %"
+    # ba_score_clf = str(round(balanced_accuracy_result_clf() * 100,2)) + " %"
+    ba_score_test = prediction[0]
+    answer = "Tomorrow's prediction for " + str(ticker) + ": "  \
         + ". We recommend you to " + recommendation + " it." \
-        +" Note that our latest SP 500 balanced accuracy score is: " + ba_score
+        +" Note that our latest balanced accuracy score for this ticker is: " + ba_score_test
+        # +" . Note that our latest SP 500 balanced accuracy score is: " + ba_score_clf
     return answer
 
 def test():
--- src/business_logic/process_query.py
+++ src/business_logic/process_query.py
@@ -2,6 +2,12 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import balanced_accuracy_score
+
 from sklearn.metrics import balanced_accuracy_score
 from datetime import date, datetime, time, timedelta
 from src.IO.fetch_stock_data import get_last_stock_price
@@ -88,4 +94,136 @@ def balanced_accuracy_result():
     results_ma['actual'] = np.where(results_ma['close'] > results_ma['previous_price'], "Buy", "Sell")
     results_ma['accuracy'] = np.where(results_ma['recommendation'] == results_ma['actual'], 1, 0)
     ba = balanced_accuracy_score(np.array(results_ma['actual']), np.array(results_ma['recommendation']))
+    return ba
+
+
+def process_data_clf(ticker):
+
+    # for ticker in sp_500:
+    df = get_last_stock_price(ticker, True)
+
+    ## Append tomorrow's date to the frame
+    nextdf = pd.DataFrame(np.zeros(len(df.columns)).reshape(1,len(df.columns)), columns = df.columns)
+    as_list = nextdf.index.tolist()
+    idx = as_list.index(0)
+    as_list[idx] =  pd.to_datetime(pd.datetime.today()+ timedelta(days=1)).strftime('%Y-%m-%d')
+    nextdf.index = as_list
+    df = df.append(nextdf)
+
+
+
+    ## Transformations: add previous close and signal
+    df['previous_close'] = df['close'].shift()
+    df['signal'] = np.where(df['close'] > df['previous_close'], 0, 1)
+    df = df.dropna()      
+
+
+    # Convert Date column to datetime
+    df.loc[:, 'date'] = pd.to_datetime(df.index,format='%Y-%m-%d')
+    # Change all column headings to be lower case, and remove spacing
+    df.columns = [str(x).lower().replace(' ', '_') for x in df.columns]
+    # # # Get month of each sample
+    df['month'] = df['date'].dt.month
+    # # Sort by datetime
+    df.sort_values(by='date', inplace=True, ascending=True)
+
+    #### Input params ##################
+    test_size = 0.1                 # proportion of dataset to be used as test set
+    cv_size = 0.3                   # proportion of dataset to be used as cross-validation set
+    num_cv = int(cv_size*len(df))
+    num_test = int(test_size*len(df))
+    #     num_test = 1
+    num_train = len(df) - num_cv - num_test
+
+    # Split into train, cv, and test
+    # train = df[:num_train].copy()
+    # cv = df[num_train:num_train+num_cv].copy()
+    #Train and CV DF - for simpler models that do not have hyperparameters it becomes and 80/20 split
+    train_cv = df[:num_train+num_cv].copy()
+    test = df[num_train+num_cv:].copy()
+
+    # Setting up and training the model
+    rfr = RandomForestClassifier(n_estimators=100, max_depth=30, bootstrap=True)
+    param_dist = dict(n_estimators=list(range(1,30)), max_depth=list(range(1,10)))
+    y_traincv = train_cv['signal']
+    X_traincv = train_cv[['previous_close', 'month']]
+    y_test = test['signal']
+    X_test = test[['previous_close', 'month']]
+
+    # Training and doing CV to find the best parameters
+    rand = RandomizedSearchCV(rfr, param_dist, cv=10, n_iter=20, random_state=0, verbose = 1)
+    rand.fit(X_traincv, y_traincv)
+    rand.cv_results_
+    n_estimators = rand.best_params_['n_estimators']
+    max_depth = rand.best_params_['max_depth']
+
+    rfr_final = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=True)
+    rfr_final.fit(X_traincv, y_traincv)
+    y_pred = rfr_final.predict(X_test)
+    ba = str(round(balanced_accuracy_score(np.array(y_test), y_pred)*100,2)) + " %"
+
+    y_pred_text = np.where(y_pred == 0, "Buy", "Sell")
+    tomorrow_recommend = y_pred_text[-1]
+    results_clf = np.array([ba,tomorrow_recommend])
+    return results_clf
+
+
+def balanced_accuracy_result_clf():
+    ## MA Final Model
+    results_ma = pd.DataFrame()
+    sp_500 = get_sp500_tickers()
+    for ticker in sp_500:
+        df = get_last_stock_price(ticker, True)
+
+        ## Transformations: add previous close and signal
+        df['previous_close'] = df['close'].shift()
+        df['signal'] = np.where(df['close'] > df['previous_close'], 0, 1)
+        df = df.dropna()      
+
+
+        # Convert Date column to datetime
+        df.loc[:, 'date'] = pd.to_datetime(df.index,format='%Y-%m-%d')
+        # Change all column headings to be lower case, and remove spacing
+        df.columns = [str(x).lower().replace(' ', '_') for x in df.columns]
+        # # # Get month of each sample
+        df['month'] = df['date'].dt.month
+        # # Sort by datetime
+        df.sort_values(by='date', inplace=True, ascending=True)
+
+        #### Input params ##################
+        test_size = 0.1                 # proportion of dataset to be used as test set
+        cv_size = 0.3                   # proportion of dataset to be used as cross-validation set
+        num_cv = int(cv_size*len(df))
+        num_test = int(test_size*len(df))
+        num_train = len(df) - num_cv - num_test
+
+        # Split into train, cv, and test
+        # train = df[:num_train].copy()
+        # cv = df[num_train:num_train+num_cv].copy()
+        #Train and CV DF - for simpler models that do not have hyperparameters it becomes and 80/20 split
+        train_cv = df[:num_train+num_cv].copy()
+        test = df[num_train+num_cv:].copy()
+
+        # Setting up and training the model
+        rfr = RandomForestClassifier(n_estimators=100, max_depth=30, bootstrap=True)
+        param_dist = dict(n_estimators=list(range(1,30)), max_depth=list(range(1,10)))
+        y_traincv = train_cv['signal']
+        X_traincv = train_cv[['previous_close', 'month']]
+        y_test = test['signal']
+        X_test = test[['previous_close', 'month']]
+
+        # Training and doing CV to find the best parameters
+        rand = RandomizedSearchCV(rfr, param_dist, cv=10, n_iter=20, random_state=0, verbose = 1)
+        rand.fit(X_traincv, y_traincv)
+        rand.cv_results_
+        n_estimators = rand.best_params_['n_estimators']
+        max_depth = rand.best_params_['max_depth']
+
+        rfr_final = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=True)
+        rfr_final.fit(X_traincv, y_traincv)
+        y_pred = rfr_final.predict(X_test)
+        results_ma = results_ma.append([[ticker,y_test[-1], y_pred[-1]]], ignore_index = True)
+
+    ba = balanced_accuracy_score(np.array(results_ma[1]), np.array(results_ma[2]))
+    
     return ba
